2937beaf672f0ff6bc6ae83544c5127cd56441aa
Jeremy <drjermy@gmail.com>
2024-05-28 16:24:08 +0100

    Generate auto-questions

--- radioUpdater/assets/js/contentScript.js
+++ radioUpdater/assets/js/contentScript.js
@@ -2,6 +2,7 @@ addElementIds()
 
 updateArticleEdit()
 updateCaseEdit()
+updateAutoQuestion()
 
 function addElementIds()
 {
@@ -17,8 +18,8 @@ function updateArticleEdit() {
 
         iterateRefTextareas(editReferenceBlock.getElementsByTagName('textarea'))
 
-        var observer = new MutationObserver( function(mutations) {
-            textareaId = mutations[0].addedNodes[0].querySelector('textarea').id
+        let observer = new MutationObserver( function(mutations) {
+            let textareaId = mutations[0].addedNodes[0].querySelector('textarea').id
             setupNewReference(textareaId)
         } )
 
@@ -32,7 +33,8 @@ function updateCaseEdit() {
     if (editReferenceBlock) {
         iterateRefTextareas(editReferenceBlock.getElementsByTagName('textarea'))
 
-        var observer = new MutationObserver( function(mutations) {
+        let observer = new MutationObserver( function(mutations) {
+            let textarea;
             if (mutations[0].target.parentElement.id === 'case-references' && mutations[0].type === 'childList') {
                 textarea = mutations[0].addedNodes[0].querySelector('textarea')
                 if (textarea) {
@@ -44,6 +46,24 @@ function updateCaseEdit() {
         } )
 
         observer.observe(editReferenceBlock, { attributes: true, childList: true, subtree: true, attributeOldValue: true })
+    }
+}
+
+function updateAutoQuestion() {
+    let relatedArticlesSection = document.getElementById('related-articles')
+
+    if (relatedArticlesSection) {
+        let questionButton = document.createElement('button')
+        questionButton.classList.add('btn')
+        questionButton.classList.add('btn-default')
+        questionButton.innerText = 'AutoQuestion'
+        questionButton.id = 'auto-question-button'
+        questionButton.disabled = true
+        questionButton.addEventListener('mouseup', getNewQuestion)
+
+        relatedArticlesSection.appendChild(questionButton)
+
+        watchForArticleUpdates()
     }
 }
 
@@ -137,6 +157,57 @@ function preloadReference(textareaId, cache) {
 
 }
 
+function getNewQuestion() {
+
+    let questionButton = document.getElementById('auto-question-button')
+    questionButton.disabled = true;
+
+    let questionTextarea = document.getElementById('for_multiple_choice_question_stem')
+    let stemContent = questionTextarea.querySelectorAll('[contenteditable="true"]')[0]
+
+    let explanationContentEditable = document.getElementById('for_multiple_choice_question_explanation').querySelectorAll('[contenteditable="true"]')[0]
+
+    let article = document.getElementsByClassName('related-article-form-li')[0].getElementsByTagName('a')[0].getAttribute("href")
+
+    article = article.substring(article.lastIndexOf('/') + 1)
+
+    chrome.runtime.sendMessage({question: article}, function ({data}) {
+
+        questionButton.disabled = false
+        if (data.error) {
+
+        } else {
+            // Set question
+            let wrappedQuestion = document.createElement('p')
+            wrappedQuestion.innerText = data.question
+            stemContent.prepend(wrappedQuestion)
+            stemContent.querySelectorAll('[data-placeholder]')[0].remove()
+
+            // Set options
+            document.getElementById('multiple_choice_question_alternatives_attributes_0_alternative_text').innerText = data.answers.a
+            document.getElementById('multiple_choice_question_alternatives_attributes_1_alternative_text').innerText = data.answers.b
+            document.getElementById('multiple_choice_question_alternatives_attributes_2_alternative_text').innerText = data.answers.c
+            document.getElementById('multiple_choice_question_alternatives_attributes_3_alternative_text').innerText = data.answers.d
+            document.getElementById('multiple_choice_question_alternatives_attributes_4_alternative_text').innerText = data.answers.e
+
+            // Set correct answer
+            let correctOptions = ['a', 'b', 'c', 'd', 'e']
+            let radioN = (correctOptions.findIndex((element) => element === data.correct));
+            document.getElementById('multiple_choice_question_alternatives_attributes_' + radioN + '_correct_true').click()
+
+            // Set explanation
+            let wrappedExplanation = document.createElement('p')
+            wrappedExplanation.innerText = data.explanation
+            explanationContentEditable.prepend(wrappedExplanation)
+            explanationContentEditable.querySelectorAll('[data-placeholder]')[0].remove()
+        }
+
+    });
+
+
+}
+
+
 function showDiff(el)
 {
     el.innerText = 'Update'
@@ -212,7 +283,7 @@ function citeItRightButton(textarea)
 
 function addCiteItRightButtonListeners(el)
 {
-    el.addEventListener("mouseover", function (event) {
+    el.addEventListener("mouseover", function () {
         if (this.innerText === "CiteItRight (hover)") {
             triggerLoad(this)
         }
@@ -238,12 +309,12 @@ function statusButton(textarea)
 
 function addStatusButtonListeners(el)
 {
-    el.addEventListener("mouseover", function(event) {
+    el.addEventListener("mouseover", function() {
         if (this.innerText === 'Review') {
             showDiff(this)
         }
     })
-    el.addEventListener("mouseout", function(event) {
+    el.addEventListener("mouseout", function() {
         if (this.innerText === 'Update') {
             hideDiff(this)
         }
@@ -332,3 +403,21 @@ let decodeHTML = function (html) {
     txt.innerHTML = html;
     return txt.value;
 };
+
+function watchForArticleUpdates()
+{
+    // Select the node that will be observed for mutations
+    document.getElementsByClassName('related-article-form-ul')[0].id = 'related-article-list';
+
+    let relatedArticleList = document.getElementById('related-article-list')
+
+    if (relatedArticleList) {
+        let observer = new MutationObserver( function(mutations) {
+            document.getElementById('auto-question-button').disabled = relatedArticleList.childElementCount < 1
+        })
+
+        observer.observe(relatedArticleList, { attributes: true, childList: true, attributeOldValue: true })
+    }
+
+}
+
